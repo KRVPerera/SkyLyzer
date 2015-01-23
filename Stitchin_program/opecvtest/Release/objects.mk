e70982da15a384aa8b4044d5d015cb42f2023031
wathmal <sasithawathmal@gmail.com>
2015-01-23 15:39:48 +0530

    image samples and cropping

---
+++ Stitchin_program/opecvtest/Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_core -lopencv_nonfree -lopencv_imgproc -lopencv_stitching -lopencv_video -lopencv_highgui
+
